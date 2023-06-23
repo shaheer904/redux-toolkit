1e0dee60b24265af7533c6bd222dca0f66bb89d4
shaheer904 <54603049+shaheer904@users.noreply.github.com>
2023-06-23 12:12:56 +0500

    first commit

---
+++ src/components/TaskList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteTodo } from '../features/todo/todo';
+const TaskList = () => {
+  const tasks = useSelector((state) => state.todo.tasks);
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>Your tasks:</h3>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>
+              {task.text}
+              <button onClick={() => handleDelete(task.id)}>delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TaskList;
